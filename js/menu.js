6d458195fbf4a9b83fa6ba1d1a2370da3725ab48
Juan Diego <jd.gonzaleza@uniandes.edu.co>
2018-02-04 14:21:43 -0500

    Avances my-web-site

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+function loadJSON(name){
+  $.getJSON("./json/"+name+".json", function(data){
+    $('#modal-title').text(data.title);
+    $('#modal-description').text(data.description);
+    $('#modal-technologies').text(data.technologies);
+    $('#modal-img').attr('src',data.img);
+    if(data.position !== undefined){ 
+      $('#modal-position').text(data.position);
+      $('#modal-position').parent().css("display","block");
+    } else {
+      $('#modal-position').parent().css("display","none");
+    }
+    $('#modal-date').text(data.dates);
+    if(data.link!== undefined){
+      $("#modal-li").css("display", "block");
+      $("#modal-link").attr('href',data.link);
+    } else {
+      $("#modal-li").css("display", "none");
+    }
+  });
+}
