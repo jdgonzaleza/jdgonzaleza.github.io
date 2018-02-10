172bd94ba275cdab420fae6e7e2e141bf7147f1c
Juan Diego <jd.gonzaleza@uniandes.edu.co>
2018-02-10 12:35:00 -0500

    contact me funcionando

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function loadJSON(name){
   $.getJSON("./json/"+name+".json", function(data){
     $('#modal-title').text(data.title);
@@ -18,36 +20,4 @@ function loadJSON(name){
       $("#modal-li").css("display", "none");
     }
   });
-}
-
-function carouselNormalization() {
-  var items   = $('#carousel-id .item'),              // grab all the slides
-      heights = [],                                   // array to store heights
-      tallest;                                        // tallest slide
-
-  if (items.length) {
-    function normalizeHeights() {
-      items.each(function() {
-        heights.push($(this).height());               // add each slide's height
-      });                                             // to the array
-
-      tallest = Math.max.apply(null, heights);        // find the largest height
-
-      items.each(function() {
-        $(this).css('min-height', tallest + 'px');    // set each slide's minimum
-      });                                             // height to the largest
-    };
-
-    normalizeHeights();
-
-    $(window).on('resize orientationchange', function() {
-      tallest = 0, heights.length = 0;               // reset the variables
-
-      items.each(function() {
-        $(this).css('min-height', '0');              // reset each slide's height
-      });
-
-      normalizeHeights();                            // run it again
-    });
-  }
 }
