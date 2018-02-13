cc75e6b08a8050d1125e9f77a55daacd30503445
Juan Diego <jd.gonzaleza@uniandes.edu.co>
2018-02-13 12:13:46 -0500

    navbar collapse funcionando

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,13 @@
+/* global $*/
+/* global WOW */
+
+
 "use strict";
+
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
+
 
 function loadJSON(name){
   $.getJSON("./json/"+name+".json", function(data){
@@ -21,3 +30,31 @@ function loadJSON(name){
     }
   });
 }
+
+(function() {
+            $(document).ready(function() {
+                return $('#contact-form').submit(function(e) {
+                    var email, message, name;
+                    name = document.getElementById('inputName');
+                    email = document.getElementById('inputEmail');
+                    message = document.getElementById('inputMessage');
+                    if (!name.value || !email.value || !message.value) {
+                        alertify.error('Please check your entries');
+                        return false;
+                    } else {
+                        $.ajax({
+                            method: 'POST',
+                            url: '//formspree.io/juandg161@gmail.com',
+                            data: $('#contact-form').serialize(),
+                            datatype: 'json'
+                        });
+                        e.preventDefault();
+                        $(this).get(0).reset();
+                        return alertify.success('Message sent');
+                    }
+                });
+            });
+
+        }).call(this);
+
+
